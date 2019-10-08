ad65af8b87bd4389826dd54e5847b68d6c89f421
JaynChoi <jaynchoi@gmail.com>
2019-10-08 14:01:39 +0900

    Updates

---
+++ precache-manifest.55799922788dcc6de8c76c9258e60532.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae5d00ac500dbf62c44f8e49ad6832c",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "d38b4c241405de71ddd9",
+    "url": "/movie_app_2019/static/css/main.93cdede2.chunk.css"
+  },
+  {
+    "revision": "9c63e566ee8166e9431f",
+    "url": "/movie_app_2019/static/js/2.6d27e71f.chunk.js"
+  },
+  {
+    "revision": "d38b4c241405de71ddd9",
+    "url": "/movie_app_2019/static/js/main.0756c09e.chunk.js"
+  },
+  {
+    "revision": "420037a5e08d7d6458d7",
+    "url": "/movie_app_2019/static/js/runtime-main.6a961451.js"
+  }
+]);
